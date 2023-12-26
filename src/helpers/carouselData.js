857162bf76f2e397fe7f3f3883df60100416f4d4
Ram Kumar <ramk41934@gmail.com>
2023-12-26 17:24:05 +0530

    Minor Changes

--- src/helpers/carouselData.js
+++ src/helpers/carouselData.js
@@ -3,12 +3,12 @@ export const carouselData = [
     title: 'Explore Time Elegance',
     description:
       'Immerse yourself in the artistry of watchmaking. Our curated selection includes an array of exquisite watches that blend timeless design with cutting-edge technology.',
-    img: '../../public/data/carouselImg/slider1.webp',
+    img: '%PUBLIC_URL%/carouselImg/slider1.webp',
   },
   {
     title: 'Luxury Beyond Time',
     description:
       'Indulge in luxury timepieces from renowned brands, each a masterpiece of engineering and aesthetics. Elevate your wrist to new levels of sophistication.',
-    img: '../../public/data/carouselImg/slider2.webp',
+    img: '%PUBLIC_URL%/carouselImg/slider2.webp',
   },
 ];
--- src/helpers/testimonialData.js
+++ src/helpers/testimonialData.js
@@ -2,16 +2,16 @@ export const testimonialData = [
   {
     title: 'Timers Villa',
     subTitle: 'Chronograph 100M',
-    img: '../../public/data/testimonialImg/Collection-1_42996848-0f7e-4b78-93a7-9847d1d1821f.webp',
+    img: '%PUBLIC_URL%/testimonialImg/Collection-1_42996848-0f7e-4b78-93a7-9847d1d1821f.webp',
   },
   {
     title: 'A friend of a Man ',
     subTitle: "Men's Maiden",
-    img: '../../public/data/testimonialImg/collection-img.webp',
+    img: '%PUBLIC_URL%/testimonialImg/collection-img.webp',
   },
   {
     title: 'Complete Mechanical ',
     subTitle: 'Thin Dial',
-    img: '../../public/data/testimonialImg/collection-3_14fa998b-de23-467a-a67c-86dc321fa7bd.webp',
+    img: '%PUBLIC_URL%/testimonialImg/collection-3_14fa998b-de23-467a-a67c-86dc321fa7bd.webp',
   },
 ];
