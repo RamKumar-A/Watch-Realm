ea02509fe365e7d44f36a57c677c94c13a92d076
Ram Kumar <ramk41934@gmail.com>
2025-05-12 19:15:21 +0530

    Fixed minor error in api.js

--- frontend/Watch-Realm/src/services/api.js
+++ frontend/Watch-Realm/src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   // baseURL: 'http://localhost:8000/api/v1',
   headers: {
     'Content-Type': 'application/json',
-    Accept: 'application/json',
+    // Accept: 'application/json',
   },
 });
 
